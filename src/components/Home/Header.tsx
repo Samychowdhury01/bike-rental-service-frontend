9f4fee896b91c91792bd6a3583f7f18dc5c7e897
samychowdhury01 <samychowdhury84@gmail.com>
2024-08-26 09:39:48 +0600

    complete hero section

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,15 +1,35 @@
-import img from '@/assets/header.png'
+import img from "@/assets/header.png";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const Header = () => {
-    return (
-        <div>
-            <div className='md:h-[768px] bg-contain bg-current flex justify-center pt-40'
-            style={{ backgroundImage: `url(${img})` }}
-            >
-            
-            </div>
+  return (
+    <div>
+      <div
+        className="md:h-[768px] bg-contain bg-current flex justify-center pt-40"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        <div className="w-1/2 space-y-5">
+          <p className="text-gradient text-5xl font-bold">
+            Escape the Ordinary, Ride Extraordinary!
+          </p>
+          <p>
+            Unlock the freedom to explore with our top-quality bikes. Whether
+            you're seeking an urban adventure or a scenic escape, we have the
+            perfect ride for you. Start your journey today—search for the ideal
+            bike and discover the thrill of two wheels. Adventure awaits just a
+            click away!
+          </p>
+          {/* for search bar */}
+          <div  className="flex justify-center gap-2">
+
+            <Input placeholder="Search for bikes" className=" shadow-2xl" />
+            <Button className="">Search</Button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
